bb814f1f6108900441bb161c26e4f12860a6d2ed
irsCooper <spock187sheldon@gmail.com>
2025-04-03 03:15:59 +0300

    fix imports

--- src/audio/dao.py
+++ src/audio/dao.py
@@ -1,5 +1,5 @@
-from audio_povepo.src.audio.model import AudioModel
-from audio_povepo.src.audio.schemas import AudioCreate, AudioUpdate
+from src.audio.model import AudioModel
+from src.audio.schemas import AudioCreate, AudioUpdate
 from src.base_dao import BaseDAO
 
 
